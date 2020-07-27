4a2cd6af8d665670f45df79e71e65be815cdff22
Juliet Goldstein <thealamogirlsings@gmail.com>
2020-07-27 10:58:58 -0400

    footer

--- src/components/Footer/Color.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Color(props) {
-    return (
-        <div>
-            My favorite color is {props.color}.
-        </div>
-    )
-}
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,47 +1,17 @@
-import React, { Component } from "react";
-import Color from "./Color";
+import React from "react";
 
-export default class index extends Component {
-
-//     //let color = "pink"
-
-//     this.state = {
-//       color: "red",
-//     };
-
-//     function colorChange() {
-//       this.setState((state) => ({ color: "green" }));
-//     }
-
-//     return (
-//       <footer>
-//         <Color color={this.state.color}></Color>
-
-//         <button type="button" className="btn" onClick={this.colorChange()}>
-//           Change Color
-//         </button>
-//       </footer>
-//     );
-
-
-constructor(props) {
-super(props);
-this.state = {
-      color: "pink",
-    };
-  }
-  colorChange = () => {
-    this.setState(state => ({ color: "green" }));
+export default function index() {
+  const color = {
+    backgroundColor: "pink",
+    color: "black",
+    position: "absolute",
+    bottom: "0",
+    width: "100%",
   };
-  render() {
-    return (
-      <div>
-        <Color color={this.state.color} />
-        <button onClick={this.colorChange} type="button">
-          Show New Color
-        </button>
-      </div>
-    );
-  }
-}
 
+  return (
+    <div className="text-center p-5" style={color}>
+      <h5>Juliet Goldstein &copy; 2020</h5>
+    </div>
+  );
+}
