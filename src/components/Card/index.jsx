c7a4eb16fe14ef878ab0f7c57526d1c5ae1847c8
Juliet Goldstein <thealamogirlsings@gmail.com>
2020-07-30 15:59:04 -0400

    filestructurechange

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
 
 export default function index() {
+  const front = "./assets/images/paintings/theswingfragonard.jpg";
+  const back = "./assets/images/paintings/frida2.jpg";
+
   return (
-    <div className="container">
-      <img src="./assets/images/paintings/theswingfragonard.jpg" />
+    <div className="container" onClick={() => {
+      
+    }}>
+      <img width="200" height="300" src={back} />
     </div>
   );
 }
