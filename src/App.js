b6594aa8c44a1a572fa88c62eb1b0a034a8778cc
Juliet Goldstein <thealamogirlsings@gmail.com>
2020-07-30 10:20:04 -0400

    board

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React from "react";
 import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-
+import Board from "./components/Board";
 function App() {
   return (
     <div className="App">
       <Header />
-
+      <Board />
       <Footer />
     </div>
   );
